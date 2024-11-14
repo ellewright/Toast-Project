c7748f78f8f4d4e6c1751c42d629921d7d1320a2
Eleanor Wright <elliewright055@gmail.com>
2024-11-14 11:06:40 -0600

    Complete project

--- after/src/App.jsx
+++ after/src/App.jsx
@@ -1,22 +1,22 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import useToast from './components/useToast'
 
 function App() {
   const inputRef = useRef(null)
-  const { addToast, removeToast, toasts } = useToast()
-
-  console.log(toasts)
+  const [id, setId] = useState()
+  const { addToast, removeToast } = useToast()
 
   function createToast() {
     if (inputRef.current.value == null || inputRef.current == null) return
-    addToast(inputRef.current.value)
+    setId(addToast(inputRef.current.value, { autoDismissTimeout: 1000 }))
   }
 
   return (
     <div className="form">
       <input type="text" ref={inputRef} />
       <button onClick={createToast}>Add</button>
+      <button onClick={() => id != null && removeToast(id)}>Remove Newest</button>
     </div>
   )
 }
--- after/src/components/ToastProvider.jsx
+++ after/src/components/ToastProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react"
+import { createPortal } from "react-dom"
 
 export const ToastContext = createContext(null)
 
@@ -16,6 +17,12 @@ export default function ToastProvider({ children }) {
         setToasts(currentToasts => {
             return [...currentToasts, { text, options, id }]
         })
+
+        if (options.autoDismiss) {
+            setTimeout(() => removeToast(id), options.autoDismissTimeout)
+        }
+
+        return id
     }
 
     function removeToast(id) {
@@ -24,7 +31,29 @@ export default function ToastProvider({ children }) {
         })
     }
 
+    const toastsByPosition = toasts.reduce((grouped, toast) => {
+        const { position } = toast.options
+        if (grouped[position] == null) {
+            grouped[position] = []
+        }
+        grouped[position].push(toast)
+
+        return grouped
+    }, {})
+
     return (
-        <ToastContext.Provider value={{ toasts, addToast, removeToast }}>{children}</ToastContext.Provider>
+        <ToastContext.Provider value={{ toasts, addToast, removeToast }}>{children}
+            {createPortal(Object.entries(toastsByPosition).map(([position, toast]) => (
+                <div key={position} className={`toast-container ${position}`}>
+                    {toasts.map(toast => (
+                        <Toast remove={() => removeToast(toast.id)} text={toast.text} key={toast.id} />
+                    ))}
+                </div>
+            )), document.getElementById("toast-container"))}
+        </ToastContext.Provider>
     )
+}
+
+function Toast({ text, remove }) {
+    return <div onClick={remove} className="toast">{text}</div>
 }
